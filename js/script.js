2e2845acc6cd363264f3118094a1feb7d69e58c3
Monk191173 <129600062+Monk191173@users.noreply.github.com>
2023-05-06 15:40:43 +0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,19 +1,27 @@
 'use strict';
-let CountOfStr=(str,substr)=>{
-return str.split(substr).length-1;
+let CountOfStr = (str, substr) => {
+    return str.split(substr).length - 1;
 }
 
 /*------------------1----------------------------------- */
-console.log('К-ть пробілів='+CountOfStr(prompt("Введіть стрічку з пробілами")," "));
+console.log('К-ть пробілів=' + CountOfStr(
+    prompt("Введіть стрічку з пробілами"), " ")
+);
 /*------------------2----------------------------------- */
-let email=prompt('Введіть email');
-if (email.indexOf("@")==-1||email.indexOf("@")==0||email.indexOf("@")==email.length-1||email.indexOf(".")==-1)
-console.log("Помилковий email")
+let email = prompt('Введіть email');
+
+if (email.indexOf("@") == -1 || email.indexOf("@") == 0 || email.indexOf("@") == email.length - 1 || email.indexOf(".") == -1)
+    console.log("Помилковий email")
 else console.log("email прийнято");
 /*-------------------3-------------------------------- */
-console.log('К-ть слів html='+CountOfStr(prompt("Введіть стрічку з html"),"html"));
+console.log('К-ть слів html=' + CountOfStr(
+    prompt("Введіть стрічку з html"), "html")
+);
 /*-------------------4-------------------------------- */
-let url=prompt('Введіть адресу (url)');
-if (url.indexOf('http://')!=-1) console.log(url.substring(7))
-else if (url.indexOf('https://')!=-1) console.log(url.substring(8))
+let url = prompt('Введіть адресу (url)');
+
+if (url.indexOf('http://') != -1)
+    console.log(url.substring(7))
+else if (url.indexOf('https://') != -1)
+    console.log(url.substring(8))
 else console.log("Невірна адреса");
